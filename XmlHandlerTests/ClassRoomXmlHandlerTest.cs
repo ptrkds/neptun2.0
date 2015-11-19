13c032dea0841d6325e24be3cf40bdc78ad72146
ukdujp <weiszkristof@gmail.com>
2015-11-19 09:21:02 +0100

    additional test methods

---
+++ XmlHandlerTests/ClassRoomXmlHandlerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neptun_2._0;
+
+namespace XmlHandlerTests
+{
+    [TestClass]
+    public class ClassRoomXmlHandlerTest
+    {
+        [TestMethod]
+        public void GetClassRoom()
+        {
+            ClassRoomXmlHandler handler = new ClassRoomXmlHandler();
+
+            ClassRoom room = handler.GetClassRoom("A1");
+
+            Console.WriteLine(ObjectDumper.Dump(room));
+        }
+    }
+}
